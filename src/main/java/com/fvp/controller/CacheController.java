c573470deaa611736e10a3995662cf6374f8076c
Nitin <nitin.goyal@Nitins-MacBook-Air.local>
2025-05-02 01:37:37 +0530

    10x.......

--- src/main/java/com/fvp/controller/CacheController.java
+++ src/main/java/com/fvp/controller/CacheController.java
@@ -3,6 +3,7 @@ package com.fvp.controller;
 import com.fvp.service.CacheService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,13 +18,13 @@ public class CacheController {
         this.cacheService = cacheService;
     }
 
-    @DeleteMapping("/clear")
+    @GetMapping("/clear")
     public ResponseEntity<String> clearAllCache() {
         cacheService.clearAllCaches();
         return ResponseEntity.ok("All caches cleared successfully");
     }
 
-    @DeleteMapping("/clear/{cacheName}")
+    @GetMapping("/clear/{cacheName}")
     public ResponseEntity<String> clearCache(@PathVariable String cacheName) {
         cacheService.clearCache(cacheName);
         return ResponseEntity.ok("Cache '" + cacheName + "' cleared successfully");
